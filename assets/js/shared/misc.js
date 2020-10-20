7ecb1573f40237a79a5bfcac464fc384dfba04cd
tobiethirbirge <tobiethirbirge@outlook.com>
2020-10-20 12:02:25 +0200

    .
    .

--- assets/js/shared/misc.js
+++ assets/js/shared/misc.js
@@ -60,3 +60,17 @@ var lightColor = getComputedStyle(document.body).getPropertyValue('--light');
   });
   $('.dropdown-toggle').dropdown()
 })(jQuery);
+
+// initialize Packery
+var $grid = $('.grid').packery({
+  itemSelector: '.grid-item',
+  // columnWidth helps with drop positioning
+  columnWidth: 100
+});
+
+// make all grid-items draggable
+$grid.find('.grid-item').each( function( i, gridItem ) {
+  var draggie = new Draggabilly( gridItem );
+  // bind drag events to Packery
+  $grid.packery( 'bindDraggabillyEvents', draggie );
+});
